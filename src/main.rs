9374757ae9c17800fd4f08455d4e9ef7fece9f9d
Frits <fritsflorentinus@gmail.com>
2023-01-24 10:44:36 -0600

    tried to make it work
    Hi Mel,
    I tried resolving the bugs, but some were tied to incorrect usage of the libraries: I could not find correct replacements for bellman::pairing and also got stuck on fixing the Circuit setup.
    Judging from the example that is on the bellman crate main page (https://docs.rs/bellman/0.13.1/bellman/index.html), it seems that there is a lot going on in setting up your own Circuit.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,38 @@
-extern crate rand;
 extern crate bellman;
-use bellman::{Circuit, Scalar};
-use bellman::groth16::{
-    create_random_proof,
-    generate_random_parameters,
-    Parameters,
-    Proof,
-    verify_proof,
-};
-use bellman::pairing::{
-    bn256::{Bn256, G1},
-};
+extern crate rand;
+extern crate bls12_381;
+use bellman::groth16;
+use bellman::groth16::{Parameters, Proof};
+use bellman::{domain::Scalar, Circuit};
+use bls12_381::Bls12;
 use rand::thread_rng;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
-fn trusted_setup() -> (Parameters<Bn256>, Proof<Bn256>) {
+fn build_trusted_setup() -> (Parameters<Bls12>, Proof<Bls12>) {
     let mut rng = thread_rng();
-    let circuit = <dyn Circuit<Scalar<Bn256>>>::new("keyring");
-    let params = generate_random_parameters(&circuit, &mut rng).unwrap();
-    let proof = create_random_proof(&params, &circuit, &mut rng).unwrap();
+    let circuit = <dyn Circuit<Scalar<Bls12>>>::new("keyring");
+    let params = groth16::generate_random_parameters(circuit, &mut rng).unwrap();
+    let proof = groth16::create_random_proof(circuit, &params, &mut rng).unwrap();  // FF: this proof never gets used anywhere
     (params, proof)
 }
 
-fn generate_proof(password: &[u8], params: &Parameters<Bn256>) -> Proof<Bn256> {
+fn generate_proof(password: &[u8], params: &Parameters<Bls12>) -> Proof<Bls12> {
     let mut rng = thread_rng();
-    create_random_proof(params, &mut rng, |p| p.input(password)).unwrap()
+    groth16::create_random_proof(|p| p.input(password), &*params, &mut rng).unwrap()
 }
 
-fn verify_password(proof: &Proof<Bn256>, params: &Parameters<Bn256>, password: &[u8]) -> bool {
-    verify_proof(params, proof, |p| p.input(password)).is_ok()
+fn verify_password(params: &Parameters<Bls12>, proof: &Proof<Bls12>, password: &[u8]) -> bool {
+    let pvk = groth16::prepare_verifying_key(&params.vk);
+    groth16::verify_proof(&pvk, &proof, |p| p.input(password)).is_ok()
 }
 
 fn authenticate_user(password: &[u8]) -> bool {
-    let (params, proof) = trusted_setup();
-    let generated_proof = generate_proof(password, &params);
-    verify_password(&generated_proof, &params, password)
+    let (params, proof) = build_trusted_setup();
+    let generated_proof = generate_proof(&password, &params);
+    verify_password(&params, &generated_proof, &password)
 }
+
 fn main() {
     println!("Enter password:");
     let mut password = String::new();
